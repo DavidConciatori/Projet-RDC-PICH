6efb10f2f7765903462303fd9ccb216053c71e42
shawnd1031 <shawnd1031@gmail.com>
2019-12-16 17:48:38 -0500

    delete

---
+++ transFoam1/transFoam1/transFoam1.cpp
@@ -0,0 +1,12 @@
+﻿// transFoam1.cpp : Defines the entry point for the application.
+//
+
+#include "transFoam1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
